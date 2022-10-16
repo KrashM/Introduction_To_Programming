100be6bd85fd3c27b1c26f037f82f8ce4a638b20
pvarna <peter.kolev2001@gmail.com>
2022-10-16 13:31:46 +0300

    minor additions

--- Week 01/Practicum/snippets/03-declarationAndInitialization.cpp
+++ Week 01/Practicum/snippets/03-declarationAndInitialization.cpp
@@ -4,7 +4,7 @@ int main ()
 {
     int declaredButNotInitializedVariable;
     int declared_but_not_initialized_variable;
-    // int declared-but-not-initialized-variable;
+    //int declared-but-not-initialized-variable;
 
     int declaredAndInitializedVariable = 10;
 
@@ -12,6 +12,8 @@ int main ()
     dummyVariable = 20; // assigning a value
 
     dummyVariable = 200;
+    dummyVariable = declaredAndInitializedVariable;
+    declaredAndInitializedVariable = dummyVariable;
 
     return 0;
 }
--- Week 01/Practicum/snippets/04-constants.cpp
+++ Week 01/Practicum/snippets/04-constants.cpp
@@ -7,4 +7,6 @@ int main ()
 
     // const double PI2;
     // PI = 2;
+
+    return 0;
 }
