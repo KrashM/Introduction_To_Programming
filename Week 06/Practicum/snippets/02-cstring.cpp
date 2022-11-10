c3ed8426a37616f2dae95bb17885c120265bf55c
pvarna <peter.kolev2001@gmail.com>
2022-11-10 22:10:37 +0200

    update code snippets

--- Week 06/Practicum/snippets/02-cstring.cpp
+++ Week 06/Practicum/snippets/02-cstring.cpp
@@ -10,9 +10,21 @@ int main ()
     char str1[MAX_CAPACITY] = "Pesho";
     char str2[MAX_CAPACITY] = "Lyubo";
 
+    std::cout << strlen(str1) << std::endl;
+    std::cout << strcmp(str1, str2) << std::endl;
+    std::cout << strcmp(str1, str1) << std::endl;
+    std::cout << strcmp(str2, str1) << std::endl;
+
     std::cout << str1 << " " << str2 << std::endl;
     swap(str1, str2);
     std::cout << str1 << " " << str2 << std::endl;
+
+    char str3[MAX_CAPACITY] = {'\0', };
+    strcat(str3, "My favourite ");
+    strcat(str3, "pop-folk singer ");
+    strcat(str3, "is Preslava");
+
+    std::cout << str3 << std::endl;
 
     return 0;
 }
