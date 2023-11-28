85aba263ce7041ad4611a06fca5bcf9b5a5acb8f
KrashM <hristo_mesi1990@abv.bg>
2023-11-28 20:47:24 +0200

    Adding a file that showcases how to use debug_new.h

---
+++ Seminars/Week 09/showcasing_debug_new.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "debug_new.h"
+
+// Not using std::nothrow, because it breaks the program.
+// My code will not throw an error anyways.
+
+int main(){
+
+    int a = 10;
+    int *ptr = &a;
+
+    printList();
+
+    std::cout << ptr << ' ' << *ptr << '\n';
+
+    ptr = new int(5);
+
+    std::cout << ptr << ' ' << *ptr << '\n';
+
+
+    std::cout << "Print 1:\n";
+    printList();
+
+    delete ptr;
+
+    std::cout << "Print 2:\n";
+    printList();
+
+    int **matrix = new int*[10];
+
+    for(size_t i = 0; i < 10; ++i)
+        matrix[i] = new int[7];
+
+    std::cout << "Print 3:\n";
+    printList();
+
+    for(size_t i = 0; i < 10; ++i)
+        delete[] matrix[i];
+
+    std::cout << "Print 4:\n";
+    printList();
+
+    delete[] matrix;
+
+    std::cout << "Print 5:\n";
+    checkMemoryLeaks();
+
+}
